651c08f1cded13b3b6977231553c036ebff1465a
Filipe Mesel Lobo Costa Cardoso <54197776+FilipeMesel@users.noreply.github.com>
2023-06-09 15:21:08 -0300

    Adicionando as funções do extrato da conta

--- projeto_conta_bancaria.py
+++ projeto_conta_bancaria.py
@@ -17,31 +17,67 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+def atualizar_extrato(valor):
+    global extrato
+    extrato += f"{valor}"
+
+def exportar_extrato():
+    global extrato
+    return extrato
+
+def depositar(quantia):
+    global extrato
+    global saldo
+    if quantia > 0:
+        status = f"Depósito:  + R$ {quantia:.2f}\n"
+        atualizar_extrato(status)
+        saldo += quantia
+        return True
+    else:
+        return False
+
+def sacar(quantia):
+    global numero_saques
+    global LIMITE_SAQUES
+    global extrato
+    global saldo
+
+    if numero_saques < LIMITE_SAQUES:
+        if quantia > 0:
+            status = f"Saque:  - R$ {quantia:.2f}\n"
+            atualizar_extrato(status)
+            saldo -= quantia
+            numero_saques+=1
+            return 0
+        else:
+            return 1
+    else:
+        return 2
+
+
 while True:
     opcao = input(menu)
 
     if opcao == "d":
         valor = float(input("Digite o quanto quer depositar:"))
-        if valor > 0:
-            extrato += f"Depósito:  + R$ {valor:.2f}\n"
-            saldo += valor
+        if depositar(valor) == True:
+            print("Operação realizada com sucesso!")
         else:
             print("Operação falhou pois o valor é inválido")
 
     elif opcao == "s":
-        if numero_saques < LIMITE_SAQUES:
-            valor = float(input("Digite o quanto quer sacar:"))
-            if valor > 0:
-                extrato += f"Saque:  - R$ {valor:.2f}\n"
-                saldo -= valor
-                numero_saques+=1
-            else:
-                print("Operação falhou pois o valor é inválido")
+
+        valor = float(input("Digite o quanto quer sacar:"))
+        status_operacao = sacar(valor)
+        if status_operacao == 0:
+            print("Operação realizada com sucesso!")
+        elif status_operacao == 1:
+            print("Erro: A quantia é menor ou igual a zero")
         else:
-            print("Você atingiu o limite de saques por hoje")
+            print("Erro: você ultrapassou a quantidade de saques")
 
     elif opcao == "e":
-        print(extrato)
+        print(exportar_extrato())
         print(f"O valor total na conta: {saldo:.2f}\n")
 
     elif opcao == "q":
